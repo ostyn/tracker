53cf103e8b59b9ef78b943fbdeb360c4f48bb07a
Jared Ostyn <ostyn@live.com>
2021-04-21 00:12:04 -0600

    Navigate to date on calendar click

--- src/resources/elements/activity/calendar-wrapper.ts
+++ src/resources/elements/activity/calendar-wrapper.ts
@@ -1,12 +1,15 @@
+import { Router } from "aurelia-router";
 import { IEntry } from "./../entry/entry.interface";
 import { autoinject, bindable } from "aurelia-framework";
 import flatpickr from "flatpickr";
 import { DateTime } from "luxon";
 import "flatpickr/dist/themes/light.css";
+import { DialogController } from "aurelia-dialog";
 @autoinject
 export class CalendarWrapper {
   @bindable public dates: Map<string, IEntry> = new Map();
   private calendar: Element;
+  constructor(private router: Router, public controller: DialogController) {}
   attached() {
     flatpickr(this.calendar, {
       inline: true,
@@ -17,6 +20,15 @@ export class CalendarWrapper {
         const entry: IEntry = this.dates.get(key);
         if (entry) dayElem.innerHTML += `<span class='event'></span>`;
       }).bind(this),
+      onChange: (a, b, c, d) => {
+        let date: Date = a[0];
+        this.router.navigateToRoute("entries", {
+          year: date.getFullYear(),
+          month: date.getMonth() + 1,
+          day: date.getDate(),
+        });
+        this.controller.cancel();
+      },
     });
   }
 }
