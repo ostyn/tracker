1b7627f083ea1ecc052aecf865037bea9fd9088c
Jared Ostyn <ostyn@live.com>
2021-05-12 23:48:22 -0600

    Playing around with dark mode, forms, and buttons

--- src/nav-bar.ts
+++ src/nav-bar.ts
@@ -1,8 +1,14 @@
 import { FirestoreCounter } from "./resources/dao/FirestoreCounter";
 import { autoinject } from "aurelia-framework";
 import { Router } from "aurelia-router";
+import nightwind from "nightwind/helper";
 @autoinject
 export class NavBar {
   FirestoreCounter = FirestoreCounter;
-  constructor(public router: Router) {}
+  constructor(public router: Router) {
+    nightwind.toggle();
+  }
+  toggleNightMode() {
+    nightwind.toggle();
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  // These paths are just examples, customize them to match your project structure
+  purge: ["./dist/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue,html,css}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms"), require("nightwind")],
+};
