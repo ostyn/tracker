78b3611682036e12b7baeab4c73c879c4559985c
Jared Ostyn <ostyn@live.com>
2021-05-29 23:30:59 -0600

    Added bullets and fixed activity loading

--- src/resources/elements/activity/activity.ts
+++ src/resources/elements/activity/activity.ts
@@ -1,12 +1,12 @@
 import { IActivity } from "./activity.interface";
 import { EventAggregator } from "aurelia-event-aggregator";
-import { computedFrom, autoinject, bindable } from "aurelia-framework";
+import { autoinject, bindable } from "aurelia-framework";
 import { ActivityService } from "resources/services/activityService";
 @autoinject
 export class Activity {
   @bindable id;
   @bindable detail;
-  activities: IActivity[] = [];
+  activity: IActivity;
   subscribers: any = [];
   constructor(
     private activityService: ActivityService,
@@ -14,23 +14,17 @@ export class Activity {
   ) {}
 
   attached() {
-    this.getActivities();
     this.subscribers.push(
-      this.ea.subscribe("activitiesUpdated", this.getActivities)
+      this.ea.subscribe("activitiesUpdated", this.loadActivity)
     );
+    this.loadActivity();
+  }
+
+  loadActivity() {
+    this.activity = this.activityService.getActivity(this.id);
   }
 
   detached() {
     this.subscribers.forEach((sub) => this.subscribers.pop().dispose());
-  }
-
-  getActivities = () => {
-    this.activities = this.activityService.getActivities();
-  };
-  @computedFrom("activities.length", "id")
-  get findActivity() {
-    return this.activities.find(
-      (activity: IActivity) => activity.id === this.id
-    );
   }
 }
