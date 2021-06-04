f5003e14f9483cb68a51f91a6b4cb4828cfd69d9
Jared Ostyn <ostyn@live.com>
2021-06-03 23:35:55 -0600

    Adding update message for service worker

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,10 +1,26 @@
 import { FrameworkConfiguration } from "aurelia-framework";
+import { Workbox } from "workbox-window";
 
 export function configure(config: FrameworkConfiguration): void {
   //config.globalResources([]);
 }
+
 if ("serviceWorker" in navigator) {
+  // Service worker setup
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/service-worker.js");
   });
+
+  // Service worker update message setup
+  const wb = new Workbox("/service-worker.js");
+  const showSkipWaitingPrompt = (event) => {
+    if (confirm("Install update?")) {
+      wb.addEventListener("controlling", (event) => {
+        window.location.reload();
+      });
+      wb.messageSkipWaiting();
+    }
+  };
+  wb.addEventListener("waiting", showSkipWaitingPrompt);
+  wb.register();
 }
