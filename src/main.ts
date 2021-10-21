d8517b0c96c4f1dc858df1452a84058039f38f54
Jared Ostyn <ostyn@live.com>
2021-10-21 23:08:44 +0100

    Adding dialog config

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ export function configure(aurelia: Aurelia): void {
   PLATFORM.moduleName("./resources/dialogs/text-prompt");
   PLATFORM.moduleName("./resources/dialogs/mood-prompt");
   PLATFORM.moduleName("./resources/dialogs/activity-info");
+  PLATFORM.moduleName("./resources/dialogs/activity-detail-prompt");
   aurelia.use
     .standardConfiguration()
     .feature(PLATFORM.moduleName("resources/index"))
