0668a0a3f0b217fe3ef1f96f6659584c28ba5a77
Jared Ostyn <ostyn@live.com>
2023-04-18 00:30:53 +0100

    Pagination for search page, better styling, debounce

--- src/resources/routes/search.route.ts
+++ src/resources/routes/search.route.ts
@@ -1,20 +1,57 @@
 import { IEntry } from "resources/elements/entry/entry.interface";
-import { autoinject } from "aurelia-framework";
+import { autoinject, bindable } from "aurelia-framework";
 import { ActivityService } from "resources/services/activityService";
 import { EntryDao } from "resources/dao/EntryDao";
 
 @autoinject
 export class SearchRoute {
-  public needle: string;
+  @bindable public needle: string;
   public entries: IEntry[];
+  public entryPage: IEntry[];
+  public currentPage = 0;
+  private pageSize = 20;
+  public lastPageIndex = 0;
+  public firstEntryIndex = 0;
+  public lastEntryIndex = 0;
 
   constructor(
     private entryDao: EntryDao,
     private activityService: ActivityService
   ) {}
+  resetState() {
+    this.entries = undefined;
+    this.entryPage = undefined;
+    this.currentPage = 0;
+    this.pageSize = 20;
+    this.lastPageIndex = 0;
+    this.firstEntryIndex = 0;
+    this.lastEntryIndex = 0;
+  }
+  needleChanged() {
+    this.currentPage = 0;
+    this.search();
+  }
+  nextPage() {
+    if (this.currentPage < this.lastPageIndex) {
+      this.currentPage++;
+      this.resliceEntries();
+      console.log(this.lastPageIndex, this.currentPage);
+    }
+  }
+  prevPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.resliceEntries();
+      console.log(this.lastPageIndex, this.currentPage);
+    }
+  }
 
   public search(): void {
     this.entryDao.getEntriesFromYearAndMonth().then((entries: IEntry[]) => {
+      if (!this.needle) {
+        this.resetState();
+        return;
+      }
       this.entries = entries.filter((entry) => {
         let regex = new RegExp(this.needle, "i");
 
@@ -30,13 +67,30 @@ export class SearchRoute {
               )
             ) ||
           entry.activitiesArray.some((activityId) =>
-            regex.test(this.activityService.activitiesMap.get(activityId).name)
+            regex.test(this.activityService.activitiesMap?.get(activityId).name)
           ) ||
           regex.test(entry.date)
         );
       });
+      this.lastPageIndex = Math.ceil(this.entries.length / this.pageSize) - 1;
+      this.resliceEntries();
     });
   }
+  private resliceEntries() {
+    if (this.entries.length) {
+      this.firstEntryIndex = this.currentPage * this.pageSize;
+      this.lastEntryIndex = Math.min(
+        (this.currentPage + 1) * this.pageSize,
+        this.entries.length
+      );
+      this.entryPage = this.entries.slice(
+        this.firstEntryIndex,
+        this.lastEntryIndex
+      );
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }
+
   isNumeric(str) {
     if (typeof str == "number") return true;
     if (typeof str !== "string") return false; // we only process strings!
